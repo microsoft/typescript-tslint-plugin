355b3da081e28aebf44c2d2a3470d97b27bf7bda
angelozerr <angelo.zerr@gmail.com>
2017-04-19 03:01:44 +0200

    Fix test

--- test/exports.spec.ts
+++ test/exports.spec.ts
@@ -5,8 +5,3 @@ test('should export a factory function', t => {
   t.equal(typeof plugin, 'function');
   t.end();
 });
-
-test('should export create function', t => {
-  t.equal(typeof plugin['create'], 'function');
-  t.end();
-})

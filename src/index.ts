7c17cde74274d51d9c47cb9d0308e8cf86de8404
Matt Bierner <matb@microsoft.com>
2018-11-14 17:34:30 -0800

    Add experimental TS services version check
    Try preventing the plugin from being loaded on invalid TS versions

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,19 @@ export = function init({ typescript }: { typescript: typeof ts_module }) {
         create(info: ts.server.PluginCreateInfo) {
             const logger = Logger.forPlugin(info);
 
+            if (!isValidTypeScriptVersion(typescript)) {
+                logger.info('Invalid typescript version detected. The TSLint plugin requires a version of TS with a services version between 0.8 and 1.0');
+                return info.languageService;
+            }
+
             return new TSLintPlugin(typescript, logger, info.project, info.config)
                 .decorate(info.languageService);
         },
     };
 };
+
+function isValidTypeScriptVersion(typescript: typeof ts_module): boolean {
+    const servicesVersion = typescript.servicesVersion;
+    const [major, minor] = servicesVersion.split('.');
+    return (+major === 0 && +minor >= 8);
+}
